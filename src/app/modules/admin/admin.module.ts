31897b34f1cb782edab4a4b2818fa01b28f88995
nguyenvanhoang0 <hoang03759@gmail.com>
2024-11-04 18:06:28 +0700

    Profile

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { IconComponent } from 'src/app/shared/components/icon/icon.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 
 
@@ -10,7 +12,9 @@ import { ProfileComponent } from './components/profile/profile.component';
   declarations: [AdminComponent,ProfileComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    NzIconModule,
+    IconComponent
   ]
 })
 export class AdminModule { }
--- src/app/modules/admin/components/profile/profile.component.ts
+++ src/app/modules/admin/components/profile/profile.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IUserInfo } from 'src/app/core/store/_auth/_auth.types';
+import { Observable } from 'rxjs';
+import { selectUserInfo } from 'src/app/core/store/_auth/_auth.selectors';
+import { Store } from '@ngrx/store';
+import { MainState } from 'src/app/core/store/_store.types';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent implements OnInit{
-ngOnInit(): void {
-  const accessToken = localStorage.getItem("userInfo");
+export class ProfileComponent {
+  userInfo$: Observable<IUserInfo | undefined> = this._store.select(selectUserInfo);
 
-}
+  constructor(private _store: Store<MainState>) {
+    this.userInfo$.subscribe()
+  }
 }
